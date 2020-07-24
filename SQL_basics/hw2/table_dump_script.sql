cc70580ffdc04642e4f71ecb102d920559c4435f
Evgeny <evgeny.hyvarinen@gmail.com>
2020-07-24 17:22:23 +0300

    fixes

--- SQL_basics/hw2/table_dump_script.sql
+++ SQL_basics/hw2/table_dump_script.sql
@@ -2,4 +2,4 @@
  * mysqldump. Создайте дамп единственной таблицы help_keyword базы 
  * данных mysql. Причем добейтесь того, чтобы дамп содержал только 
  * первые 100 строк таблицы. */
-mysqldump -u root -p --where="true limit 100" mysql --tables help_keyword
+mysqldump -u root -p --where="true limit 100" mysql --tables help_keyword > help_dump_100.sql
